38c82665b72b6a28dd8c7248dc982e787cefebd5
Le5ya <liliatolstyh@yandex.ru>
2024-01-14 16:46:49 +0300

    store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { UseDispatch, useSelector } from "react-redux";
 import { Header } from "./views/Header/Header";
 import { Footer } from "./views/Footer/Footer";
 import { Main } from "./views/Main/Main";
+import { useEffect } from "react";
+import { fetchAccessToken } from "./store/auth/auth.slice";
 
-const App = () => (
-  <>
-    <Header />
-    <Main />
-    <Footer />
-  </>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const { accessToken, loading } = useSelector((state) => state.auth);
 
+  useEffect(() => {
+    if (!accessToken) {
+      dispatch(fetchAccessToken());
+    }
+  }, [dispatch, accessToken]);
+
+  return (
+    <>
+      <Header />
+      {!loading && accessToken ? <Main /> : <div>Загрузка...</div>}
+      <Footer />
+    </>
+  );
+};
 export default App;

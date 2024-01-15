c1b640cdc085dee18e32acbd397a8cea74789102
Le5ya <liliatolstyh@yandex.ru>
2024-01-15 15:58:47 +0300

    slice for goods

---
+++ src/store/goods/goods.slice.js
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchGoods = createAsyncThunk(
+  "categories/fetchGoods",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const token = state.auth.accessToken;
+
+    const response = await fetch("https://koff-api.vercel.app/api/products", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Не удалось найти товар");
+    }
+    return response.json();
+  },
+);
+
+const initialState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+const goodsSlice = createSlice({
+  name: "goods",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGoods.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchGoods.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchGoods.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+export default goodsSliceSlice.reducer;

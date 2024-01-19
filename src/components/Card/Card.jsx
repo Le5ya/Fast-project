6248b677340e7b58400caadbaa1f01b28a78b8d3
Le5ya <liliatolstyh@yandex.ru>
2024-01-20 02:16:07 +0300

    styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -11,7 +11,6 @@ export const Card = () => {
   const [mainSwiper, setMainSwiper] = useState(null);
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const { productId } = useParams();
-  console.log(productId);
   return (
     <section className={s.card}>
       <Container className={s.container}>
@@ -21,50 +20,52 @@ export const Card = () => {
             <Swiper
               modules={[Thumbs]}
               thumbs={{ swiper: thumbsSwiper }}
-              onSwipeer={setMainSwiper}
+              onSwiper={setMainSwiper}
               spaceBetween={10}>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="кресло" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
             </Swiper>
+            <div className={s.pagination}>
+              <button onClick={() => mainSwiper.slideNext()}>next</button>
+              <button onClick={() => mainSwiper.slidePrev()}>prev</button>
+            </div>
           </div>
           <div className={s.sliderThumbnails}>
             <Swiper
               onSwiper={setThumbsSwiper}
               modules={[Thumbs]}
               watchSlidesProgress
-              spaceBetween={14}>
-              slidesPerView={4}
+              spaceBetween={14}
+              slidesPerView={4}>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="Кресло" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src="./img/photo.jpg" alt="" />
+                <img src="/public/img/photo.jpg" alt="" />
               </SwiperSlide>
             </Swiper>
-            <button onClick={() => mainSwiper.slideNext()}>next</button>
-            <button onClick={() => mainSwiper.slidePrev()}>prev</button>
           </div>
         </div>
         <div className={s.info}>
@@ -73,6 +74,51 @@ export const Card = () => {
         </div>
         <div className={s.characteristic}>
           <h3>Общие характеристики</h3>
+          <ul>
+            <li className={s.item}>
+              <p>Коллекция</p>
+              <p>Мирсаж</p>
+            </li>
+            <li className={s.item}>
+              <p>Производитель</p>
+              <p>Россия</p>
+            </li>
+            <li className={s.item}>
+              <p>Гарантия</p>
+              <p>18 мес.</p>
+            </li>
+            <li className={s.item}>
+              <p>Срок службы</p>
+              <p>5 лет</p>
+            </li>
+            <li className={s.item}>
+              <p>Цвет</p>
+              <p>Желтый</p>
+            </li>
+            <li className={s.item}>
+              <p>Макс. нагрузка</p>
+              <p>130 кг</p>
+            </li>
+          </ul>
+        </div>
+        <div className={s.buttons}>
+          <button className={s.btn}>В корзину</button>
+          <button className={s.favorite} aria-label="Избранное">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M8.41337 13.8733C8.18671 13.9533 7.81337 13.9533 7.58671 13.8733C5.65337 13.2133 1.33337 10.46 1.33337 5.79332C1.33337 3.73332 2.99337 2.06665 5.04004 2.06665C6.25337 2.06665 7.32671 2.65332 8.00004 3.55998C8.67337 2.65332 9.75337 2.06665 10.96 2.06665C13.0067 2.06665 14.6667 3.73332 14.6667 5.79332C14.6667 10.46 10.3467 13.2133 8.41337 13.8733Z"
+                fill="white"
+                stroke="#1C1C1C"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
         </div>
       </Container>
     </section>
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth.slice";
 import categoriesReducer from "./categories/categories.slice";
 import goodsReducer from "./goods/goods.slice";
+import productReducer from "./product/product.slice.js";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     categories: categoriesReducer,
     goods: goodsReducer,
+    product: productReducer,
   },
 });
